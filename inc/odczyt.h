ea5e824e82b7e0abb6ce7ff614cf75e8c945929c
KacSz123 <56486180+KacSz123@users.noreply.github.com>
2022-03-01 23:37:37 +0100

    zx

---
+++ inc/odczyt.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+#define MAX 1024            /* Maksymalny rozmiar wczytywanego obrazu */
+#define DL_LINII 1024      /* Dlugosc buforow pomocniczych */
+
+/************************************************************************************
+ * Funkcja wczytuje obraz PGM z pliku do tablicy       	       	       	       	    *
+ *										                                            *
+ * \param[in] plik_we uchwyt do pliku z obrazem w formacie PGM			            *
+ * \param[out] obraz_pgm tablica, do ktorej zostanie zapisany obraz		            *
+ * \param[out] wymx szerokosc obrazka						                        *
+ * \param[out] wymy wysokosc obrazka						                        *
+ * \param[out] szarosci liczba odcieni szarosci					                    *
+ * \return liczba wczytanych pikseli						                        *
+ ************************************************************************************/
+
+int czytaj(FILE *plik_we,int obraz_pgm[][MAX],int *wymx,int *wymy, int *szarosci); 
+
+
+
+/* Wyswietlenie obrazu o zadanej nazwie za pomoca programu "display"   */
+void wyswietl(char *n_pliku); 
+
+int zapisz(FILE *fp, int obraz_pgm[MAX][MAX],int wymx,int wymy, int szarosci);
+void Negatyw(int (*obraz_pgm)[MAX][MAX], int wymx, int wymy, int  szarosci);
+void Konturowanie(int (*o_pgm)[MAX][MAX], int wymx, int wymy); 
+void Progowanie(int (*o_pgm)[MAX][MAX], int wymx, int wymy, int szarosci ,int prog);
+void Rozmycie_pion(int (*o_pgm)[MAX][MAX], int wymx, int wymy, int promien);
+void Rozmycie_poz(int (*o_pgm)[MAX][MAX], int wymx, int wymy, int promien);
