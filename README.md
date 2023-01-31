1642656c9683cbf3d769e650959958b07ea5b4b1
Kacper Szczepanowski <kacperszcz159@gmail.com>
2023-01-31 21:30:21 +0100

    add ppm img

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Przetwarzanie-Obraz-w-1
+# Image proccessing. 
+
+## 
+Program made for classes at WUST for processing pgm and ppm images. 
--- inc/image.h
+++ inc/image.h
@@ -1,6 +1,6 @@
 /**
  * @file image.h
- * @author Kaper Szczepanowski http://panamint.kcir.pwr.edu.pl/~kszczep2/
+ * @author Kaper Szczepanowski kacperszcz159@gmail.com
  * @brief 
  * @version 0.3
  * @date 2022-10-06
@@ -15,14 +15,23 @@
 #include<string.h>
 #include<stdlib.h>
 #include<math.h>
-
+#include<stdbool.h>
 typedef enum ErroType{FILE_ERROR, OTHER_ERROR};
 
+typedef int ppm[3];
+//optional typedef struct ppm{int _rgb[3];}ppm; 
+union imgPixel
+{
+    int _pixel;
+    ppm _pixelRGB;
+
+};
 typedef struct Image{
 
 char _magickNumber[2];
 int _dimY;
 int _dimX;
+//future impPixel** _img;
 int** _img;
 int _shades;
 char* _name;
@@ -32,7 +41,7 @@ char* _name;
 ///
 ///
 
-inline void InitImage(Image *Img)
+inline void LoadImage(Image *Img)
 {
     Img=(Image*)malloc(sizeof(Img));
     Img->_img=(int**)malloc(sizeof(int)*(Img->_dimX)*(Img->_dimY));
@@ -43,12 +52,13 @@ inline void FreeImageSpace(Image *Img)
     free(Img);
 }
 ////////
-
-/**
+// bool a = true;
+/*!
  * @brief 
  * 
- * @param ImgInput 
- * @param ImgOutput 
+ * @param file 
+ * @param Img 
+ * @return int 
  */
 int ReadFile(FILE *file, Image *Img);
 void WriteFile(FILE *file, const Image Img);
